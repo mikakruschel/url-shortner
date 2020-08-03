a3f13a8a7f2d4496ec8766c6667fb502e6c2d2d1
Miká Kruschel <20423069+mikakruschel@users.noreply.github.com>
2020-08-03 14:48:15 +0200

    Added functionality

---
+++ static/index.js
@@ -0,0 +1,33 @@
+const respP = document.querySelector('#responseP');
+
+document.querySelector('form').addEventListener('submit', e => {
+  fetch('/url', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      short: document.querySelector('#inputShort').value,
+      long: document.querySelector('#inputLong').value,
+    }),
+  })
+      .then(res => res.json())
+      .then(data => {
+        if (data.errorMessage) {
+          respP.innerHTML = data.errorMessage || 'Unkown error';
+          respP.classList.add('text-danger');
+          respP.classList.remove('text-primary');
+        } else {
+          respP.innerHTML = `Url was shortened to <a class="link-success" href="/${data.short}">mü.xyz/${data.short}</a>`;
+          respP.classList.add('text-primary');
+          respP.classList.remove('text-danger');
+        }
+      })
+      .catch(err => {
+        respP.innerHTML = err.message || err || 'Unkown error';
+        respP.classList.add('text-primary');
+        respP.classList.remove('text-danger');
+      });
+  e.preventDefault();
+});
