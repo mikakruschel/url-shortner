f9d09fe4f5c9d3b9fbeed43609e3e0956de58859
Miká Kruschel <20423069+mikakruschel@users.noreply.github.com>
2020-08-04 11:57:37 +0200

    Revert "move to heroku"
    This reverts commit 6dcae747ad113a0a210abead3e88192b50a95bdf.

--- server/app.js
+++ server/app.js
@@ -7,18 +7,20 @@ require('dotenv').config();
 
 const middlewares = require('./middlewares');
 const router = require('./router');
+// const apiRouter = require('./api');
 
 const app = express();
-app.set('trust proxy', 1);
+// app.set('trust proxy', 1);
 
 app.use(secure);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(express.static('static'));
+// if (process.env.NODE_ENV == 'development') app.use(express.static('static'));
 
 app.use('/', router);
+// app.use('/api', apiRouter);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
